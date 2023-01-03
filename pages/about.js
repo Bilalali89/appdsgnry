d8d5f89e1a3d08520395f8c1ab376a55c7ab7ee2
Syed Bilal Ali <bilalali89@gmail.com>
2023-01-03 11:34:23 +0500

    about us page

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,6 @@
 import React, { Component } from 'react';
 import NavbarTwo from '../components/Layouts/NavbarTwo';
 import PageBanner from '../components/Common/PageBanner';
-import AboutContent from '../components/About/AboutContent';
-import FunFactsTwo from '../components/Common/FunFactsTwo';
-import TeamStyleThree from '../components/Common/TeamStyleThree';
-import Feedback from '../components/Common/Feedback';
-import Partner from '../components/Common/Partner';
-import CtaAreaTwo from '../components/Common/CtaAreaTwo';
 import Footer from '../components/Layouts/Footer';
 import HomeBottomContact from '../components/Common/HomeBottomContact';
 import AboutHeaderSection from '../components/About/AboutHeaderSection';
